5ec81767db3736eaab9d23b9f8df3582eaafdcd6
tomoya.terai <tomoya.terai01@gmail.com>
2021-08-01 21:58:49 +0900

    add_post_slug_page

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,73 @@
 <template>
   <v-container fluid>
-    <template v-if="posts.length">
-      <ul v-for="(post, i) in posts" :key="i">
-        <li>{{ post.fields.title }}</li>
-        <ul>
-          <v-img
-            :src="post.fields.image.fields.file.url"
-            :alt="post.fields.image.fields.title"
-            :aspect-ratio="16/9"
+    <v-row
+      justify="center"
+    >
+    <v-col
+      cols="12"
+      sm="11"
+      md="10"
+      xl="8"
+    >
+      <v-row v-if="posts.length">
+        <v-col
+          v-for="(post, i) in posts"
+          :key="i"
+          cols="12"
+          sm="6"
+          lg="4"
+          xl="3"
+        >
+          <v-card
             max-width="400"
-            max-height="225"/>
-          <li>{{ post.fields.body }}</li>
-          <li>{{ post.fields.publishDate }}</li>
-        </ul>
-      </ul>
-    </template>
-    <template v-else>
-      投稿された記事はありません。
-    </template>
+            class="mx-auto"
+          >
+            <v-img
+              :src="setEyeCatch(post).url"
+              :alt="setEyeCatch(post).title"
+              :aspect-ratio="16/9"
+              max-height="200"
+              class="white--text"
+            >
+              <v-card-title class="align-end fill-height font-weight-bold">
+                {{ post.fields.title }}
+              </v-card-title>
+            </v-img>
+
+            <v-card-text>
+              {{ post.fields.publishDate }}
+            </v-card-text>
+            
+            <v-list-item three-line style="min-height: unset;">
+              <v-list-item-subtitle>
+                {{ post.fields.body }}
+              </v-list-item-subtitle>
+            </v-list-item>
+
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                text
+                color="primary"
+                :to="linkTo(post)"
+              >
+                この記事をみる
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <div v-else class="text-center">
+        投稿された記事はありません
+      </div>
+    </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import client from '~/plugins/contentful'
+import client from '@/plugins/contentful'
+import { mapGetters } from 'vuex'
 
 export default {
   async asyncData({ env }) {
@@ -32,6 +77,12 @@ export default {
       order: '-fields.publishDate'
     }).then(res => (posts = res.items)).catch(console.error)
     return { posts }
+  },
+  computed: {
+    linkTo: () => (obj) => {
+      return { name: 'posts-slug', params: { slug: obj.fields.slug } }
+    },
+    ...mapGetters(['setEyeCatch'])
   }
 }
 </script>
