150097a1445214ab810a19d2ac6a2440cb03b27e
tomoya.terai <tomoya.terai01@gmail.com>
2021-08-08 12:45:01 +0900

    top_page_add_tags

--- pages/index.vue
+++ pages/index.vue
@@ -50,6 +50,31 @@
                                 </v-list-item-subtitle>
                             </v-list-item>
 
+                            <v-card-text
+                                style="height: 64px;"
+                            >
+                                <template v-if="post.fields.tags">
+                                    <v-chip
+                                        v-for="(tag) in post.fields.tags"
+                                        :key="tag.sys.id"
+                                        to="#"
+                                        small
+                                        label
+                                        outlined
+                                        class="ma-1"
+                                    >
+                                        <v-icon
+                                            left
+                                            size="18"
+                                            color="grey"
+                                        >
+                                            mdi-label
+                                        </v-icon>
+                                        {{ tag.fields.name }}
+                                    </v-chip>
+                                </template>
+                            </v-card-text>
+                            
                             <v-card-actions>
                                 <v-spacer />
                                 <v-btn text color="primary" :to="linkTo('posts', post)">
