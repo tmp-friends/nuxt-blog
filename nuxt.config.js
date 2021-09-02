8d6e793c8d79d3f74436c548224ee8d09202b554
tomoya.terai <tomoya.terai01@gmail.com>
2021-09-02 17:21:03 +0900

    setup_metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,14 +6,24 @@ require('dotenv').config()
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-blog',
+    title: process.env.SITE_NAME,
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
+      prefix: 'og: http://ogp.me/ns#'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'こめかみの備忘録【プログラミング、デザイン、イラスト】' },
+      { hid: 'og:site_name', property: 'og:site_name', content: process.env.SITE_NAME },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: process.env.BASE_URL },
+      { hid: 'og:title', property: 'og:title', content: process.env.SITE_NAME },
+      { hid: 'og:description', property: 'og:description', content: 'こめかみの備忘録【プログラミング、デザイン、イラスト】' },
+      { hid: 'og:image', property: 'og:image', content: '~/assets/images/defaultEyeCatch.jpg' },
+      { hid: 'twitter:card', name: 'twitter:card', content: "summary_large_image" },
+      { hid: 'twitter:site', name: 'twitter:site', content: process.env.TWITTER_ACCOUNT },
+
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
