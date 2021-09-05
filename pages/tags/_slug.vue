21c3df05a91b7f57a23770e7d103fb0ccf28c98c
tomoya.terai <tomoya.terai01@gmail.com>
2021-09-05 12:44:42 +0900

    set_tag/_slug

--- pages/tags/_slug.vue
+++ pages/tags/_slug.vue
@@ -3,21 +3,28 @@
         <breadcrumbs :add-items="addBreads" />
         <v-container>
             <h1>{{ tag.fields.name }}</h1>
-            <div
+            <v-list-item
                 v-for="(post, i) in relatedPosts"
                 :key="i"
+                :to="$store.getters.linkTo('posts', post)"
             >
-                {{ post.fields.title }}
-            </div>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        {{ post.fields.title }}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
         </v-container>
     </div>
 </template>
 
 <script>
 import { mdiTagOutline } from '@mdi/js'
+import { mapGetters } from 'vuex';
 
 export default {
     computed: {
+        ...mapGetters('linkTo'),
         addBreads() {
             return [{ icon: mdiTagOutline, text: 'タグ一覧', to: '/tags' }]
         }
